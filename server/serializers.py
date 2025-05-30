2d5eafd3553b99936646c8c4901d98402fe01f0f
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-30 17:45:31 +0530

    Add ChatSerializer and enhance UserWithChatsSerializer to include chats

--- server/serializers.py
+++ server/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import User
+from . import models
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Chat
+        fields = "__all__"
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = models.User
         fields = '__all__'
+
+class UserWithChatsSerializer(serializers.ModelSerializer):
+    chats = ChatSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.User
+        fields = ['user_id', 'user_name','chats']

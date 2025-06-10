ea2fd0bc50140e61165b510d0cbe84c400329450
Yatik1 <yatiksrivastava1@gmail.com>
2025-06-10 12:55:25 +0530

    Fix urlpatterns formatting by adding missing commas and ensuring proper structure

--- server/urls.py
+++ server/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("api/users/", views.create_user),
     path("api/chat/", views.create_chat),
     path("api/get_users", views.get_users),
-    path("api/messages/<chat_id>", views.chat_messages)
+    path("api/messages/<chat_id>", views.chat_messages),
+    path("api/rename/<chat_id>", views.update_chat_name),
+    path("api/delete/chat/<chat_id>", views.delete_chat)
 ]
